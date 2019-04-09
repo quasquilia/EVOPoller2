9068510dd3c4e2cf7a8ddf22fd56a47a1e63d7d3
alessandro.guarascio <alessandro.guarascio@wolterskluwer.com>
2019-04-09 15:16:02 +0200

    OpenApiWeather

--- src/main/kotlin/com/guarascio/evopoller2/weather/adapters/wunderground/OpenWeatherMapAdapter.kt
+++ src/main/kotlin/com/guarascio/evopoller2/weather/adapters/wunderground/OpenWeatherMapAdapter.kt
@@ -3,7 +3,7 @@ package com.guarascio.evopoller2.weather.adapters.wunderground
 import org.springframework.web.client.RestTemplate
 import java.net.URI
 
-class WUndergroundAdapter {
+class OpenWeatherMapAdapter {
 
 
 
@@ -12,8 +12,8 @@ class WUndergroundAdapter {
         fun main(args: Array<String>) {
 
             var restTemplate = RestTemplate()
-            val wUndergroundCondition = restTemplate.getForObject(URI("http://api.wunderground.com/api/d645214de6bc3045/conditions/q/IT/Capannori.json"),
-                    WUndergroundCondition::class.java)
+            val wUndergroundCondition = restTemplate.getForObject(URI("http://api.openweathermap.org/data/2.5/weather?q=Capannori,it&appid=f2befaac7be67d76e55a42e552f6b098"),
+                    OpenWeatherMapCondition::class.java)
 
             println(wUndergroundCondition)
 
---
+++ src/main/kotlin/com/guarascio/evopoller2/weather/adapters/wunderground/OpenWeatherMapCondition.kt
@@ -0,0 +1,21 @@
+package com.guarascio.evopoller2.weather.adapters.wunderground
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class OpenWeatherMapCondition(
+        @JsonProperty("id") var id: Int,
+        @JsonProperty("name") var name: String,
+        @JsonProperty("main") var main: OpenWeatherMapMain,
+        @JsonProperty("weather") var weather: Array<OpenWeatherMapWeather>
+) {
+    data class OpenWeatherMapMain(
+            @JsonProperty("temp") var tempK: Double,
+            @JsonProperty("temp_min") var tempKMin: Double,
+            @JsonProperty("temp_max") var tempKMax: Double
+    )
+
+    data class OpenWeatherMapWeather(
+            @JsonProperty("main") var main: String,
+            @JsonProperty("description") var description: String
+    )
+}
+
